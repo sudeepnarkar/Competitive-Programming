f787b14a5f8e33f4b093ed84e4bf5cb28f51d0cb
sudeepnarkar <>
2019-01-13 22:07:15 -0500

    Added FizzBuzz and modified gitignore

---
+++ src/Leetcode/FizzBuzz.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                list.add("FizzBuzz");
+            }else if(i%3==0){
+                list.add("Fizz");
+            }else if(i%5==0){
+                list.add("Buzz");
+            }else{
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}
