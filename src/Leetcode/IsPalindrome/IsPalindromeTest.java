c385ef717d4a1dd57e2a2e8312f34e3315444885
sudeepnarkar <>
2019-03-30 15:43:46 -0400

    IsPalindrome - Removed unused imports

--- src/Leetcode/IsPalindrome/IsPalindromeTest.java
+++ src/Leetcode/IsPalindrome/IsPalindromeTest.java
@@ -1,11 +1,8 @@
 package Leetcode.IsPalindrome;
 
+
 import org.junit.Test;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 public class IsPalindromeTest {
