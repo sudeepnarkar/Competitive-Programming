4d0316f927ef4f0d80c11b13ec67c73358a3b085
sudeepnarkar <>
2019-04-06 18:24:52 -0400

    Added AC solution for Remove Element and LinkedList CRUD operations sample.

---
+++ src/Leetcode/RemoveElements.java
@@ -0,0 +1,35 @@
+package Leetcode;
+
+public class RemoveElements {
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+
+   /* Remove all elements from a linked list of integers that have value val.
+
+            Example:
+
+    Input:  1->2->6->3->4->5->6, val = 6
+    Output: 1->2->3->4->5*/
+
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        while(head!=null && head.val == val){
+            head = head.next;
+        }
+        ListNode curr = head;
+        while(curr!=null && curr.next!=null){
+            if (curr.next.val == val){
+                curr.next = curr.next.next;
+            }else{
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
