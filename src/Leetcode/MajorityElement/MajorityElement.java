6ff6ead671e155e0309ddc14fed4c153a668cf06
sudeepnarkar <narkarsudeep1@gmail.com>
2020-10-18 16:34:09 -0400

    Updated Majority Element

--- src/Leetcode/MajorityElement/MajorityElement.java
+++ src/Leetcode/MajorityElement/MajorityElement.java
@@ -1,8 +1,8 @@
 package Leetcode.MajorityElement;
 
-import java.util.Arrays;
 import java.util.HashMap;
 
+//https://leetcode.com/problems/majority-element
 class MajorityElement {
 
     /*Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
@@ -18,8 +18,27 @@ class MajorityElement {
     Input: [2,2,1,1,1,2,2]
     Output: 2*/
 
-    public static int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+    public int majorityElement(int[] nums) {
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len = nums.length / 2;
+        int majorityElem = -1;
+        for (int n : nums) {
+            if (map.containsKey(n)) {
+                if (map.get(n) == len) {
+                    majorityElem = n;
+                    break;
+                }
+                map.put(n, map.get(n) + 1);
+            } else {
+                map.put(n, 1);
+            }
+        }
+        //majority is guaranteed to be present in the input, so this line will never be reached.
+        return majorityElem;
     }
 }
