99ab8337554865161a069f4128214dc3aa6cd8d9
sudeepnarkar <narkarsudeep1@gmail.com>
2020-10-17 22:46:49 -0400

    BinaryTreeRightSideView iterative solution

---
+++ src/Leetcode/BinaryTreeRightSideView.java
@@ -0,0 +1,76 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author : Sudeep Narkar
+ * @since : 10/17/2020, Sat, 10:40 PM
+ **/
+
+public class BinaryTreeRightSideView {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+    /**
+     * Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+     *
+     * Input: [1,2,3,null,5,null,4]
+     * Output: [1, 3, 4]
+     * Explanation:
+     *
+     *    1            <---
+     *  /   \
+     * 2     3         <---
+     *  \     \
+     *   5     4       <---
+     *
+     */
+
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> rightSideList = new ArrayList<>();
+        if (root == null) {
+            return rightSideList;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.remove();
+                if (i == (size - 1)) {
+                    rightSideList.add(node.val);
+                }
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
+        return rightSideList;
+    }
+}
