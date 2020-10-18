dc08a231e1e44c2e7c5041e4107c2a05c554cead
sudeepnarkar <narkarsudeep1@gmail.com>
2020-10-18 13:08:41 -0400

    Binary Tree Postorder traversal recursive and iterative

--- src/Leetcode/BinaryTreePostOrderTraversal.java
+++ src/Leetcode/BinaryTreePostOrderTraversal.java
@@ -1,9 +1,12 @@
 package Leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
+
+//https://leetcode.com/problems/binary-tree-postorder-traversal
 public class BinaryTreePostOrderTraversal {
 
     public class TreeNode {
@@ -16,13 +19,29 @@ public class BinaryTreePostOrderTraversal {
         }
     }
 
-   // Given a binary tree, return the postorder traversal of its nodes' values.
+    // Given a binary tree, return the postorder traversal of its nodes' values.
 
-    public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> postorderTraversalRecursive(TreeNode root) {
+
+        if (root == null) {
+            return list;
+        }
+
+        postorderTraversalRecursive(root.left);
+        postorderTraversalRecursive(root.right);
+        list.add(root.val);
+        return list;
+
+    }
+
+    public List<Integer> postorderTraversalIterative(TreeNode root) {
         LinkedList<Integer> ans = new LinkedList<>();
         Stack<TreeNode> stack = new Stack<>();
-        if (root == null)
+        if (root == null) {
             return ans;
+        }
         stack.push(root);
         while (!stack.isEmpty()) {
             TreeNode cur = stack.pop();
