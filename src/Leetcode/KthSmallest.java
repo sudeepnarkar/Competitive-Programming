931fb0bd3c224d222e51d62132eba70505c95067
sudeepnarkar <narkarsudeep1@gmail.com>
2020-10-19 09:08:07 -0400

    Kth Smallest Element in a BST Iterative

--- src/Leetcode/KthSmallest.java
+++ src/Leetcode/KthSmallest.java
@@ -1,7 +1,9 @@
 package Leetcode;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 public class KthSmallest {
 
     public class TreeNode {
@@ -14,25 +16,71 @@ public class KthSmallest {
         }
     }
 
-    //Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+    /**
+     *
+     *Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: root = [3,1,4,null,2], k = 1
+     *    3
+     *   / \
+     *  1   4
+     *   \
+     *    2
+     * Output: 1
+     * Example 2:
+     *
+     * Input: root = [5,3,6,2,4,null,null,1], k = 3
+     *        5
+     *       / \
+     *      3   6
+     *     / \
+     *    2   4
+     *   /
+     *  1
+     * Output: 3
+     */
 
     public int kthSmallest(TreeNode root, int k) {
         ArrayList<Integer> list = new ArrayList<>();
         if (root == null) {
             return 0;
         }
-        list = traverse(root, list);
+        list = kthSmallestRecursive(root, list);
         return list.get(k - 1);
     }
 
     // In-order traversal of BST gives elements in non-decreasing order.
-    ArrayList<Integer> traverse(TreeNode root, ArrayList<Integer> list) {
+    ArrayList<Integer> kthSmallestRecursive(TreeNode root, ArrayList<Integer> list) {
         if (root == null) {
             return list;
         }
-        traverse(root.left, list);
+        kthSmallestRecursive(root.left, list);
         list.add(root.val);
-        traverse(root.right, list);
+        kthSmallestRecursive(root.right, list);
         return list;
+    }
+
+    // In-order traversal of BST gives elements in non-decreasing order.
+    public int kthSmallestIterative(TreeNode root, int k) {
+
+        Stack<TreeNode> st = new Stack<>();
+        int res = 0;
+        while (root != null || !st.isEmpty()) {
+            while (root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            if (--k == 0) {
+                res = root.val;
+                break;
+            }
+            root = root.right;
+        }
+        return res;
     }
 }
