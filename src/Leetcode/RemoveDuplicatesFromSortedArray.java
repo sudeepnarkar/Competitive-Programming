3ae34d7b2a63b292fdc32d05f4d1310527dfa2c9
sudeepnarkar <narkarsudeep1@gmail.com>
2020-10-23 18:50:30 -0400

    Remove Duplicates from Sorted Array

--- src/Leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/Leetcode/RemoveDuplicatesFromSortedArray.java
@@ -1,23 +1,53 @@
 package Leetcode;
 
+
+/**
+ * @author : Sudeep Narkar
+ * @since : 10/23/2020, Fri, 6:49 PM
+ **/
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array
 public class RemoveDuplicatesFromSortedArray {
 
-    /*Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
-
-    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-
-    Example 1:
-
-    Given nums = [1,1,2],
-
-    Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
-
-    It doesn't matter what you leave beyond the returned length.*/
+    /**
+     * Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+     * <p>
+     * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+     * <p>
+     * Clarification:
+     * <p>
+     * Confused why the returned value is an integer but your answer is an array?
+     * <p>
+     * Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+     * <p>
+     * Internally you can think of this:
+     * <p>
+     * // nums is passed in by reference. (i.e., without making a copy)
+     * int len = removeDuplicates(nums);
+     * <p>
+     * // any modification to nums in your function would be known by the caller.
+     * // using the length returned by your function, it prints the first len elements.
+     * for (int i = 0; i < len; i++) {
+     * print(nums[i]);
+     * }
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [1,1,2]
+     * Output: 2, nums = [1,2]
+     * Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.
+     * Example 2:
+     * <p>
+     * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+     * Output: 5, nums = [0,1,2,3,4]
+     * Explanation: Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively. It doesn't matter what values are set beyond the returned length.
+     */
 
     public int removeDuplicates(int[] nums) {
         int index = 0;
-        for(int n:nums){
-            if (index==0|| n>nums[index-1]){
+        for (int n : nums) {
+            if (index == 0 || n > nums[index - 1]) {
                 nums[index++] = n;
             }
         }
