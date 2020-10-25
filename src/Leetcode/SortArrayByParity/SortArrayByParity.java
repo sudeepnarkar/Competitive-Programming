e2242e41862c20230e1ece3d38748777c48a3868
sudeepnarkar <narkarsudeep1@gmail.com>
2020-10-24 21:44:56 -0400

    Sort Array By Parity updated with constant space

--- src/Leetcode/SortArrayByParity/SortArrayByParity.java
+++ src/Leetcode/SortArrayByParity/SortArrayByParity.java
@@ -1,31 +1,37 @@
 package Leetcode.SortArrayByParity;
 
+
+/**
+ * @author : Sudeep Narkar
+ * @since : 10/24/2020, Sat, 9:38 PM
+ **/
+
+//https://leetcode.com/problems/sort-array-by-parity/
 public class SortArrayByParity {
 
 
-   /* Given an array A of non-negative integers,
-    return an array consisting of all the even elements of A, followed by all the odd elements of A.
-
-    You may return any answer array that satisfies this condition.
-
-            Example 1:
-
-    Input: [3,1,2,4]
-    Output: [2,4,3,1]
-    The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.*/
+    /**
+     * Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+     * <p>
+     * You may return any answer array that satisfies this condition.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [3,1,2,4]
+     * Output: [2,4,3,1]
+     * The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+     */
 
 
     public int[] sortArrayByParity(int[] A) {
-        int i = 0;
-        int j = A.length-1;
-        int []res = new int[A.length];
-        for (int h=0;h<A.length;h++){
-            if ((A[h]%2)==0){
-                res[i++] = A[h];
-            }else{
-                res[j--]  = A[h];
+        int index = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0) {
+                int temp = A[i];
+                A[i] = A[index];
+                A[index++] = temp;
             }
         }
-        return res;
+        return A;
     }
 }
