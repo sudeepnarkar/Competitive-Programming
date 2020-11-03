1158687d7e7ab97e800eddc42c8624ac458067b0
sudeepnarkar <narkarsudeep1@gmail.com>
2020-11-03 16:38:13 -0500

    Two Sum II - Input array is sorted updated

--- src/Leetcode/TwoSumInputArrayIsSorted.java
+++ src/Leetcode/TwoSumInputArrayIsSorted.java
@@ -1,40 +1,57 @@
 package Leetcode;
 
+/**
+ * @author : Sudeep Narkar
+ * @since : 11/3/2020, Tue, 16:36 PM
+ **/
+
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
 public class TwoSumInputArrayIsSorted {
 
+    /**
+     * Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+     * <p>
+     * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+     * <p>
+     * Note:
+     * <p>
+     * Your returned answers (both index1 and index2) are not zero-based.
+     * You may assume that each input would have exactly one solution and you may not use the same element twice.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: numbers = [2,7,11,15], target = 9
+     * Output: [1,2]
+     * Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: numbers = [2,3,4], target = 6
+     * Output: [1,3]
+     * <p>
+     * Example 3:
+     * <p>
+     * Input: numbers = [-1,0], target = -1
+     * Output: [1,2]
+     */
 
-
-   /* Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
-
-    The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
-
-            Note:
-
-    Your returned answers (both index1 and index2) are not zero-based.
-    You may assume that each input would have exactly one solution and you may not use the same element twice.
-    Example:
-
-    Input: numbers = [2,7,11,15], target = 9
-    Output: [1,2]
-    Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.*/
-   
     public int[] twoSum(int[] numbers, int target) {
-        int[] res = new int[2];
+
         int low = 0;
         int high = numbers.length - 1;
-        int sum = 0;
+
         while (low < high) {
-            sum = numbers[low] + numbers[high];
+            int sum = numbers[low] + numbers[high];
             if (sum == target) {
-                res[0] = low + 1;
-                res[1] = high + 1;
-                break;
-            } else if (sum < target) {
-                low++;
-            } else {
+                return new int[]{low + 1, high + 1};
+            } else if (sum > target) {
                 high--;
+            } else {
+                low++;
             }
         }
-        return res;
+        return new int[]{-1, -1};
     }
 }
