bbc26e8494957628634de92ce63d9ffb2ab5a166
sudeepnarkar <narkarsudeep1@gmail.com>
2020-11-07 22:07:28 -0500

    Search in a Binary Search Tree updated

--- src/Leetcode/SearchBST.java
+++ src/Leetcode/SearchBST.java
@@ -1,38 +1,62 @@
 package Leetcode;
 
+//https://leetcode.com/problems/search-in-a-binary-search-tree/
 public class SearchBST {
 
     public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode(int x) { val = x; }
-  }
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-    /*Given the root node of a binary search tree (BST) and a value.
-    You need to find the node in the BST that the node's value equals the given value.
-    Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.*/
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+     * <p>
+     * For example,
+     * <p>
+     * Given the tree:
+     * 4
+     * / \
+     * 2   7
+     * / \
+     * 1   3
+     * <p>
+     * And the value to search: 2
+     * <p>
+     * You should return this subtree:
+     * <p>
+     * 2
+     * / \
+     * 1   3
+     * <p>
+     * In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+     * <p>
+     * Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+     */
 
     public TreeNode searchBSTRecursive(TreeNode root, int val) {
-        if(root == null){
+        if (root == null) {
             return null;
         }
-        if(root.val == val){
+        if (root.val == val) {
             return root;
-        }
-        else if (root.val<val){
-            return searchBSTRecursive(root.right,val);
-        }else{
-            return searchBSTRecursive(root.left,val);
+        } else if (root.val < val) {
+            return searchBSTRecursive(root.right, val);
+        } else {
+            return searchBSTRecursive(root.left, val);
         }
     }
 
     public TreeNode searchBSTIterative(TreeNode root, int val) {
 
-        while(root!=null && root.val!=val){
-            if (root.val>val){
+        while (root != null && root.val != val) {
+            if (root.val > val) {
                 root = root.left;
-            }else{
+            } else {
                 root = root.right;
             }
         }
