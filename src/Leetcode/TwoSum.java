56562df577b4077c3ecfc8b7b0a4eed93f12d47b
sudeepnarkar <narkarsudeep1@gmail.com>
2020-11-09 21:59:57 -0500

    Two Sum updated

--- src/Leetcode/TwoSum.java
+++ src/Leetcode/TwoSum.java
@@ -1,33 +1,47 @@
 package Leetcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
+//https://leetcode.com/problems/two-sum/
 public class TwoSum {
 
-/*
-    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-    You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-            Example:
-
-    Given nums = [2, 7, 11, 15], target = 9,
-
-    Because nums[0] + nums[1] = 2 + 7 = 9,
-            return [0, 1]*/
-
+    /**
+     * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+     * <p>
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * <p>
+     * You can return the answer in any order.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [2,7,11,15], target = 9
+     * Output: [0,1]
+     * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: nums = [3,2,4], target = 6
+     * Output: [1,2]
+     * <p>
+     * Example 3:
+     * <p>
+     * Input: nums = [3,3], target = 6
+     * Output: [0,1]
+     */
 
     public int[] twoSum(int[] nums, int target) {
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int [] res = new int[2];
-        for(int i = 0;i<nums.length;i++){
-            if(map.containsKey(target-nums[i])){
-                res[0] = map.get(target-nums[i]);
-                res[1] = i;
-                break;
-            }else{
-                map.put(nums[i],i);
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{i, map.get(diff)};
+            } else {
+                map.put(nums[i], i);
             }
         }
         return res;
