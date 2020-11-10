c84abaae1898152a44fbad5fcf3d0a619047f12c
sudeepnarkar <narkarsudeep1@gmail.com>
2020-11-09 22:02:56 -0500

    Average of Levels in Binary Tree updated

--- src/Leetcode/AverageOfLevelsInBinaryTree/AverageOfLevels.java
+++ src/Leetcode/AverageOfLevelsInBinaryTree/AverageOfLevels.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+//https://leetcode.com/problems/average-of-levels-in-binary-tree
 public class AverageOfLevels {
 
 
@@ -18,20 +19,32 @@ public class AverageOfLevels {
         }
     }
 
-    // Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+    /**
+     * Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input:
+     * 3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
+     * Output: [3, 14.5, 11]
+     * Explanation:
+     * The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+     */
 
     public List<Double> averageOfLevels(TreeNode root) {
-        ArrayList<Double> list = new ArrayList<>();
+        List<Double> list = new ArrayList<>();
         if (root == null) {
             return list;
         }
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        double size = 0.0;
-        double sum = 0.0;
-        while (!q.isEmpty()) {
-            size = q.size();
-            sum = 0.0;
+        while (q.size() != 0) {
+            double sum = 0;
+            double size = q.size();
             for (int i = 0; i < size; i++) {
                 TreeNode node = q.remove();
                 if (node.left != null) {
