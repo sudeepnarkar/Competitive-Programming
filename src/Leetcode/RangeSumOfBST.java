ccae4045a09fff6c95f9e85130c215c0212ab8b2
sudeepnarkar <narkarsudeep1@gmail.com>
2020-11-11 07:47:26 -0500

    Range Sum of BST updated

--- src/Leetcode/RangeSumOfBST.java
+++ src/Leetcode/RangeSumOfBST.java
@@ -1,5 +1,12 @@
 package Leetcode;
 
+
+/**
+ * @author : Sudeep Narkar
+ * @since : 11/11/2020, Tue, 7:46 AM
+ **/
+
+//https://leetcode.com/problems/range-sum-of-bst/
 public class RangeSumOfBST {
 
 
@@ -14,9 +21,21 @@ public class RangeSumOfBST {
     }
 
 
-    /*Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
-
-    The binary search tree is guaranteed to have unique values.*/
+    /**
+     * Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+     * Output: 32
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+     * Output: 23
+     */
 
 
     public int rangeSumBST(TreeNode root, int L, int R) {
