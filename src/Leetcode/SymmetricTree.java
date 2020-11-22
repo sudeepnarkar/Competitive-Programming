f8b9f688387642f585420547ed2d102ef9c41717
sudeepnarkar <narkarsudeep1@gmail.com>
2020-11-22 07:08:58 -0500

    Symmetric Tree Updated

--- src/Leetcode/SymmetricTree.java
+++ src/Leetcode/SymmetricTree.java
@@ -1,5 +1,9 @@
 package Leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+//https://leetcode.com/problems/symmetric-tree
 public class SymmetricTree {
 
 
@@ -13,59 +17,87 @@ public class SymmetricTree {
         }
     }
 
+    /**
+     * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+     * <p>
+     * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+     * <p>
+     * 1
+     * / \
+     * 2   2
+     * / \ / \
+     * 3  4 4  3
+     * <p>
+     * <p>
+     * <p>
+     * But the following [1,2,2,null,3,null,3] is not:
+     * <p>
+     * 1
+     * / \
+     * 2   2
+     * \   \
+     * 3    3
+     *
+     * @param root
+     * @return true if the tree is symmetric or return false
+     */
 
-    public boolean isSymmetric(TreeNode root) {
 
+    public boolean isSymmetricRecursive(TreeNode root) {
         if (root == null) {
             return true;
         }
 
-        if (root.left == null && root.right == null) {
-            return true;
-        }
-
-        if (root.left == null || root.right == null) {
-            return false;
-        }
-
-        if (root.left.val != root.right.val) {
-            return false;
-        }
-
         return isSymmetricUtil(root.left, root.right);
-
     }
 
 
     boolean isSymmetricUtil(TreeNode left, TreeNode right) {
 
 
-        if (left == null && right == null) {
-            return true;
-        }
-
+        // If either left or right is null, check if both are null
         if (left == null || right == null) {
-            return false;
+            return left == right;
         }
 
+        // Left and Right are not null, so check if their values are equal
         if (left.val != right.val) {
             return false;
         }
 
-        if (left.left != null && right.right != null) {
-            if (left.left.val != right.right.val) {
-                return false;
-            }
-
-        }
-
-        if (left.right != null && right.left != null) {
-            if (left.right.val != right.left.val) {
-                return false;
-            }
-
-        }
-
+        // repeat for left and right subtree recursively
         return isSymmetricUtil(left.left, right.right) && isSymmetricUtil(left.right, right.left);
+    }
+
+    public boolean isSymmetricIterative(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        q.offer(left);
+        q.offer(right);
+
+        while (q.size() != 0) {
+            left = q.poll();
+            right = q.poll();
+            // If both left and right are null, keeping removing from the queue
+            if (left == null && right == null) {
+                continue;
+            }
+            // If either left or right is null or if their values are not equal return false
+            if (left == null || right == null || left.val != right.val) {
+                return false;
+            }
+            q.offer(left.left);
+            q.offer(right.right);
+            q.offer(left.right);
+            q.offer(right.left);
+        }
+        return true;
     }
 }
