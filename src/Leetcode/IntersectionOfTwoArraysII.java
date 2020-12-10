0f0dd4476d3765393bc24dabff916cf777f13552
sudeepnarkar <narkarsudeep1@gmail.com>
2020-12-09 22:14:06 -0500

    Intersection of Two Arrays II updated

--- src/Leetcode/IntersectionOfTwoArraysII.java
+++ src/Leetcode/IntersectionOfTwoArraysII.java
@@ -2,46 +2,53 @@ package Leetcode;
 
 import java.util.ArrayList;
 
+//https://leetcode.com/problems/intersection-of-two-arrays-ii
 public class IntersectionOfTwoArraysII {
 
 
+    /**
+     * Given two arrays, write a function to compute their intersection.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+     * Output: [2,2]
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+     * Output: [4,9]
+     * <p>
+     * Note:
+     * <p>
+     * Each element in the result should appear as many times as it shows in both arrays.
+     * The result can be in any order.
+     *
+     * @param nums1
+     * @param nums2
+     * @return intersection of two arrays
+     */
 
-    /*Given two arrays, write a function to compute their intersection.
-
-            Example 1:
-
-    Input: nums1 = [1,2,2,1], nums2 = [2,2]
-    Output: [2,2]
-    Example 2:
-
-    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-    Output: [4,9]
-    Note:
-
-    Each element in the result should appear as many times as it shows in both arrays.
-    The result can be in any order.
-*/
-
-
+    // Time Complexity = O(n)
+    // Space Complexity = O(n)
     public int[] intersect(int[] nums1, int[] nums2) {
 
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> res = new ArrayList<>();
-        for(int num:nums1){
-            list1.add(num);
-        }
-        for(int num:nums2){
-            if(list1.contains(num)){
-                res.add(num);
-                list1.remove((Integer)num);
-            }
-        }
-        int size = res.size();
-        int [] nums = new int[size];
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> resList = new ArrayList<>();
 
-        for(int i=0;i<size;i++){
-            nums[i] = res.get(i);
+        for (int n : nums1) {
+            list.add(n);
         }
-        return nums;
+
+        for (int n : nums2) {
+            if (list.contains(n)) {
+                resList.add(n);
+                list.remove(list.indexOf(n));
+            }
+
+        }
+        int[] arr = resList.stream().mapToInt(i -> i).toArray();
+        return arr;
+
     }
 }
