c224472e2b180eeef1824414deee74a8faf6d41f
sudeepnarkar <narkarsudeep1@gmail.com>
2021-01-03 17:02:30 -0500

    Move Length of the last word to the Leetcode package

--- src/Leetcode/LengthOfLastWord.java
+++ src/Leetcode/LengthOfLastWord.java
@@ -1,22 +1,44 @@
 package Leetcode;
 
+/**
+ * @author : Sudeep Narkar
+ * @since : 12/2/2020, Wed, 8:24 PM
+ **/
+
+//https://leetcode.com/problems/length-of-last-word/
 public class LengthOfLastWord {
 
+    /**
+     * Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+     * <p>
+     * A word is a maximal substring consisting of non-space characters only.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:inPalindrome
+     * Input: s = "Hello World"
+     * Output: 5
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: s = " "
+     * Output: 0
+     *
+     * @param s
+     * @return length of the last word in the string. If the last word does not exist, return 0.
+     */
 
-    /*Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
-
-    If the last word does not exist, return 0.
-
-    Note: A word is defined as a character sequence consists of non-space characters only.
-
-            Example:
-
-    Input: "Hello World"
-    Output: 5*/
-
+    // Time complexity = O(n)
+    // Space complexity = O(n)
     public int lengthOfLastWord(String s) {
-        String[] strArr = s.trim().split(" ");
-        int len = strArr.length;
-        return strArr[len-1].length();
+
+        s = s.trim();
+        String[] arr = s.split(" ");
+
+        if (arr.length == 0) {
+            return 0;
+        }
+
+        return arr[arr.length - 1].length();
     }
 }
