7a4b0fef45486c92e0ffc445c83497a5294f6c41
sudeepnarkar <narkarsudeep1@gmail.com>
2021-01-05 21:30:37 -0500

    Remove Linked List Elements updated

--- src/Leetcode/RemoveElements.java
+++ src/Leetcode/RemoveElements.java
@@ -1,32 +1,43 @@
 package Leetcode;
 
+//https://leetcode.com/problems/remove-linked-list-elements
 public class RemoveElements {
 
     public class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
-  }
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
 
 
-   /* Remove all elements from a linked list of integers that have value val.
+    /**
+     * Remove all elements from a linked list of integers that have value val.
+     * <p>
+     * Example:
+     * <p>
+     * Input:  1->2->6->3->4->5->6, val = 6
+     * Output: 1->2->3->4->5
+     *
+     * @param head
+     * @param val
+     * @return
+     */
 
-            Example:
-
-    Input:  1->2->6->3->4->5->6, val = 6
-    Output: 1->2->3->4->5*/
-
-
+    // Time Complexity = O(N), where N is the length of Linked List
+    // Space Complexity = O(1)
     public ListNode removeElements(ListNode head, int val) {
 
-        while(head!=null && head.val == val){
+        while (head != null && head.val == val) {
             head = head.next;
         }
         ListNode curr = head;
-        while(curr!=null && curr.next!=null){
-            if (curr.next.val == val){
+        while (curr != null && curr.next != null) {
+            if (curr.next.val == val) {
                 curr.next = curr.next.next;
-            }else{
+            } else {
                 curr = curr.next;
             }
         }
