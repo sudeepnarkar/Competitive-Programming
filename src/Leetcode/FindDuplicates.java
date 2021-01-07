12322e734ec363b85446a0f3faa832231f733d26
sudeepnarkar <narkarsudeep1@gmail.com>
2021-01-06 22:22:25 -0500

    Find Duplicates

--- src/Leetcode/FindDuplicates.java
+++ src/Leetcode/FindDuplicates.java
@@ -3,30 +3,37 @@ package Leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+//https://leetcode.com/problems/find-all-duplicates-in-an-array
 public class FindDuplicates {
 
-   /* Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+    /**
+     * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+     * <p>
+     * Find all the elements that appear twice in this array.
+     * <p>
+     * Could you do it without extra space and in O(n) runtime?
+     * <p>
+     * Example:
+     * Input:
+     * [4,3,2,7,8,2,3,1]
+     * <p>
+     * Output:
+     * [2,3]
+     *
+     * @param nums
+     * @return List of integer consisting of elements that appear twice
+     */
 
-    Find all the elements that appear twice in this array.
-
-    Could you do it without extra space and in O(n) runtime?
-
-    Example:
-    Input:
-            [4,3,2,7,8,2,3,1]
-
-    Output:
-            [2,3]*/
-
-
+    // Time Complexity = O(N), where N is the length of nums
+    // Time Complexity = O(N/2), where N is the length of nums
     public List<Integer> findDuplicates(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             int index = Math.abs(nums[i]);
-            if(nums[index-1]<0){
+            if (nums[index - 1] < 0) {
                 list.add(index);
-            }else{
-                nums[index-1] = -nums[index-1];
+            } else {
+                nums[index - 1] = -nums[index - 1];
             }
         }
         return list;
