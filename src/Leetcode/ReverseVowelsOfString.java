6fa931032724cd81818c9db4d13058a13ea6817f
sudeepnarkar <narkarsudeep1@gmail.com>
2021-01-18 21:47:49 -0500

    Reverse Vowels of a String updated

--- src/Leetcode/ReverseVowelsOfString.java
+++ src/Leetcode/ReverseVowelsOfString.java
@@ -1,52 +1,43 @@
 package Leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
+//https://leetcode.com/problems/reverse-vowels-of-a-string
 public class ReverseVowelsOfString {
 
-   /* Write a function that takes a string as input and reverse only the vowels of a string.
+    /**
+     * Write a function that takes a string as input and reverse only the vowels of a string.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: "hello"
+     * Output: "holle"
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: "leetcode"
+     * Output: "leotcede"
+     * <p>
+     * Note:
+     * The vowels does not include the letter "y".
+     *
+     * @param a
+     * @return string which reverses only the vowels of a string.
+     */
 
-            Example 1:
-
-    Input: "hello"
-    Output: "holle"
-    Example 2:
-
-    Input: "leetcode"
-    Output: "leotcede"*/
-
-
-
-    public static boolean isVowel(char a){
-        switch(a){
-            case ('a') : return true;
-            case ('e') : return true;
-            case ('i') : return true;
-            case ('o') : return true;
-            case ('u') : return true;
-            case ('A') : return true;
-            case ('E') : return true;
-            case ('I') : return true;
-            case ('O') : return true;
-            case ('U') : return true;
-            default : return false;
-        }
-    }
-
+    //Time Complexity = O(N), where N is the length of the String
+    //Space Complexity = O(1)
     public String reverseVowels(String s) {
 
-        char [] arr = s.toCharArray();
+        char[] arr = s.toCharArray();
         int i = 0;
-        int j = arr.length-1;
+        int j = arr.length - 1;
 
-        while(i<j){
+        while (i < j) {
 
-            if(!isVowel(arr[i])){
+            if (!isVowel(arr[i])) {
                 i++;
                 continue;
             }
-            if(!isVowel(arr[j])){
+            if (!isVowel(arr[j])) {
                 j--;
                 continue;
             }
@@ -54,8 +45,36 @@ public class ReverseVowelsOfString {
             char temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
-            i++;j--;
+            i++;
+            j--;
         }
         return String.valueOf(arr);
+    }
+
+    public static boolean isVowel(char a) {
+        switch (a) {
+            case ('a'):
+                return true;
+            case ('e'):
+                return true;
+            case ('i'):
+                return true;
+            case ('o'):
+                return true;
+            case ('u'):
+                return true;
+            case ('A'):
+                return true;
+            case ('E'):
+                return true;
+            case ('I'):
+                return true;
+            case ('O'):
+                return true;
+            case ('U'):
+                return true;
+            default:
+                return false;
+        }
     }
 }
