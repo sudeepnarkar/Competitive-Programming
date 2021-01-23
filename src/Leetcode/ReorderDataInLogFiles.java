57ac12a7e3f8c76e2dcd30ef0d15f82561187d92
sudeepnarkar <narkarsudeep1@gmail.com>
2021-01-23 12:58:29 -0500

    Reorder Data in Log Files updated

--- src/Leetcode/ReorderDataInLogFiles.java
+++ src/Leetcode/ReorderDataInLogFiles.java
@@ -54,12 +54,12 @@ public class ReorderDataInLogFiles {
             public int compare(String log1, String log2) {
 
                 int index1 = log1.indexOf(" ");
-
                 String id1 = log1.substring(0, index1);
                 String main1 = log1.substring(index1 + 1);
 
-                String id2 = log2.substring(0, index1);
-                String main2 = log2.substring(index1 + 1);
+                int index2 = log2.indexOf(" ");
+                String id2 = log2.substring(0, index2);
+                String main2 = log2.substring(index2 + 1);
 
                 boolean isDigit1 = Character.isDigit(main1.charAt(0));
                 boolean isDigit2 = Character.isDigit(main2.charAt(0));
@@ -68,6 +68,8 @@ public class ReorderDataInLogFiles {
                     int value = main1.compareTo(main2);
                     if (value == 0) {
                         return id1.compareTo(id2);
+                    } else {
+                        return value;
                     }
                 }
 
