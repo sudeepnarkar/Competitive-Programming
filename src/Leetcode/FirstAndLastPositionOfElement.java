e1e01ac5d67378dff1e471c376aa38d93066bfa0
sudeepnarkar <narkarsudeep1@gmail.com>
2021-02-09 19:45:33 -0500

    First And Last Position Of Element updated

--- src/Leetcode/FirstAndLastPositionOfElement.java
+++ src/Leetcode/FirstAndLastPositionOfElement.java
@@ -1,25 +1,47 @@
 package Leetcode;
 
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
 public class FirstAndLastPositionOfElement {
-/*
-    Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+    /**
+     * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+     * <p>
+     * If target is not found in the array, return [-1, -1].
+     * <p>
+     * Follow up: Could you write an algorithm with O(log n) runtime complexity?
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [5,7,7,8,8,10], target = 8
+     * Output: [3,4]
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: nums = [5,7,7,8,8,10], target = 6
+     * Output: [-1,-1]
+     * <p>
+     * Example 3:
+     * <p>
+     * Input: nums = [], target = 0
+     * Output: [-1,-1]
+     * <p>
+     * <p>
+     * <p>
+     * Constraints:
+     * <p>
+     * 0 <= nums.length <= 105
+     * -109 <= nums[i] <= 109
+     * nums is a non-decreasing array.
+     * -109 <= target <= 109
+     *
+     * @param nums
+     * @param target
+     * @return an array of left and right extreme index of the target in the array
+     */
 
-    If target is not found in the array, return [-1, -1].
-
-    Follow up: Could you write an algorithm with O(log n) runtime complexity?
-
-    Example 1:
-
-    Input: nums = [5,7,7,8,8,10], target = 8
-    Output: [3,4]
-    Example 2:
-
-    Input: nums = [5,7,7,8,8,10], target = 6
-    Output: [-1,-1]
-    Example 3:
-
-    Input: nums = [], target = 0*/
-
+    //Time Complexity = O(logN), where N is the length of nums
+    //Space Complexity = O(1)
     public int[] searchRange(int[] nums, int target) {
 
         int indexLeft = getExtremeIndex(nums, target, true);
@@ -41,17 +63,17 @@ public class FirstAndLastPositionOfElement {
      * @param nums
      * @param target
      * @param findLeftIndex
-     * @return
+     * @return  left or the right extreme index value for the target based on findLeftIndex
      */
     public int getExtremeIndex(int nums[], int target, boolean findLeftIndex) {
 
         int low = 0;
-        int high = nums.length;
+        int high = nums.length - 1;
 
-        while (low < high) {
+        while (low <= high) {
             int med = low + (high - low) / 2;
             if (target < nums[med] || (nums[med] == target && findLeftIndex)) {
-                high = med;
+                high = med - 1;
             } else {
                 low = med + 1;
             }
