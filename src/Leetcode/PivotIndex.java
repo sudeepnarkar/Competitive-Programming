c26e137f44764aa4cde846bc52691321a49a015a
sudeepnarkar <narkarsudeep1@gmail.com>
2021-03-12 20:30:21 -0500

    Pivot Index updated

--- src/Leetcode/PivotIndex.java
+++ src/Leetcode/PivotIndex.java
@@ -1,35 +1,37 @@
 package Leetcode;
 
+
+//https://leetcode.com/problems/find-pivot-index
 public class PivotIndex {
 
-   /* Given an array of integers nums, write a method that returns the "pivot" index of this array.
+    /**
+     * Given an array of integers nums, calculate the pivot index of this array.
+     * <p>
+     * The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+     * <p>
+     * If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+     * <p>
+     * Return the leftmost pivot index. If no such index exists, return -1.
+     *
+     * @param nums
+     * @return the leftmost pivot index. If no such index exists, return -1.
+     */
 
-    We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.
-
-    If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
-
-            Example 1:
-
-    Input:
-    nums = [1, 7, 3, 6, 5, 6]
-    Output: 3
-    Explanation:
-    The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to the sum of numbers to the right of index 3.
-    Also, 3 is the first index where this occurs.
-*/
+    //Time Complexity = O(N), where N is the length of nums
+    //Space Complexity = O(1)
     public int pivotIndex(int[] nums) {
 
         int sum = 0;
-        for(int n:nums){
-            sum+=n;
+        for (int n : nums) {
+            sum += n;
         }
 
         int currSum = 0;
-        for(int i=0;i<nums.length;i++){
-            if(currSum == ((sum-nums[i])-currSum)){
+        for (int i = 0; i < nums.length; i++) {
+            if (currSum == ((sum - nums[i]) - currSum)) {
                 return i;
             }
-            currSum+=nums[i];
+            currSum += nums[i];
         }
 
         return -1;
